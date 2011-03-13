7b3d14d3117921df001db8c80259589cb09f40ab
eps <dev@epscylonb.com>
2011-03-13 20:35:10 +0000

    first version

---
+++ main.lua
@@ -0,0 +1,131 @@
+function love.load()
+	width = 800
+	height = 600
+	
+	grid = getNewGrid()
+	for x = 1,width/10 do
+		grid[x] = {}
+		for y = 1,height/10 do
+			if x > 20 and x < 40 and y > 20 and y < 40 then
+				--girid[x][y] = 0;			
+				grid[x][y] = math.random(0,1)
+			end
+			--grid[x][y] = math.random(0,1)
+		end
+	end
+	
+	--grid[20][20] = 1;
+	--grid[21][21] = 1;
+	--grid[20][21] = 1;
+	--grid[20][22] = 1;
+	
+end
+
+-- initialises an empty grid
+function getNewGrid()
+	local newGrid = {}
+	
+	for x = 1,width/10 do
+		newGrid[x] = {}
+		for y = 1,height/10 do
+			newGrid[x][y] = 0;		
+		end
+	end
+	
+	return newGrid
+end
+
+function love.update(dt)
+	love.timer.sleep(100)
+	
+	local newGrid = getNewGrid()
+	
+	for x = 1, width/10 do
+		for y = 1, height/10 do
+			ln = getNeighbours(x,y)
+			if ln < 2 then newGrid[x][y] = 0 end
+			if ln > 1 and ln < 4 and grid[x][y] == 1 then newGrid[x][y] = 1 end
+			if ln > 3 then newGrid[x][y] = 0 end
+			if ln == 3 then newGrid[x][y] = 1 end
+		end
+	end
+	
+	if love.mouse.isDown('l') then
+		local x = love.mouse.getX()
+		local y = love.mouse.getY()
+		x = x - (x%10) 
+		y = y - (y%10)
+		--love.graphics.rectangle('fill', x, y, 10, 10)
+		grid[x/10][y/10] = 1
+	end
+	grid = newGrid
+end
+
+function getNeighbours(x,y)
+	
+--	x-1,y
+--	x+1,y
+--	x,y+1
+--	x,y-1
+--	x-1,y-1
+--	x+1,y+1
+--	x+1,y-1
+--	x-1,y+1
+
+	ln = 0 -- living neighbours
+	if grid[x-1] ~= nil and grid[x-1][y] == 1 then ln = ln + 1 end
+	if grid[x+1] ~= nil and grid[x+1][y] == 1 then ln = ln + 1 end
+	if grid[x][y-1] ~= nil and grid[x][y-1] == 1 then ln = ln + 1 end
+	if grid[x][y+1] ~= nil and grid[x][y+1] == 1 then ln = ln + 1 end
+	if grid[x+1] ~= nil and grid[x+1][y+1] ~= nil and grid[x+1][y+1] == 1 then ln = ln + 1 end
+	if grid[x-1] ~= nil and grid[x-1][y-1] ~= nil and grid[x-1][y-1] == 1 then ln = ln + 1 end
+	if grid[x+1] ~= nil and grid[x+1][y-1] ~= nil and grid[x+1][y-1] == 1 then ln = ln + 1 end
+	if grid[x-1] ~= nil and grid[x-1][y+1] ~= nil and grid[x-1][y+1] == 1 then ln = ln + 1 end
+	
+	return ln
+	
+end
+
+function love.draw()
+		
+	for x = 1,800 do
+		love.graphics.line(x * 10, 0, x * 10 , height) 
+	end
+	for y = 1,600 do
+		love.graphics.line(0, y * 10, width, y * 10)
+	end
+	
+	for x = 1,width/10 do
+		for y = 1,height/10 do
+			if grid[x][y] == 1 then
+				love.graphics.rectangle('fill', x * 10, y * 10, 10, 10)
+			end			
+			-- love.graphics.polygon(fill, x, y, x + 10, y + 10)
+		end
+	end
+	
+    local x = love.mouse.getX()
+    local y = love.mouse.getY()
+	x = x - (x%10) 
+	 y = y - (y%10)
+	love.graphics.print("The mouse is at (" .. x .. "," .. y .. ")", 50, 50)
+	--love.graphics.print("Height and width are (" .. height .. "," .. width .. ")", 50, 50)
+	-- love.graphics.rectangle('fill', 10, 10, 10, 10)
+	-- love.graphics.polygon('fill', 0, 0, 0, 10, 10, 10, 10, 10)
+	-- love.graphics.polygon("fill", 0,0,10,10)
+	-- love.graphics.setBackgroundColor(200,200,200)
+end
+
+function love.mousereleased(x, y, button)
+		
+	 x = x - (x%10) 
+	 y = y - (y%10)
+		--love.graphics.print("The mouse is at (" .. x .. "," .. y .. ")", 50, 50)
+	if button == 'l' then
+		--grid[x][y] = 1
+		love.graphics.rectangle('fill', x, y, 10, 10)
+		grid[x/10][y/10] = 1
+      --fireSlingshot(x,y) -- this totally awesome custom function is defined elsewhere
+	  
+	end
+end
