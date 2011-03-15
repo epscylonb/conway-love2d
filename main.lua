72519795a1360b7016c8b82198eada8571a11ca2
eps <dev@epscylonb.com>
2011-03-15 19:54:35 +0000

    added some constants, changed the size of the window

--- main.lua
+++ main.lua
@@ -1,15 +1,20 @@
 require 'socket'
 
 function love.load()
-	width = 800
-	height = 600
+		
+	width = 1024
+	height = 768
+	CELL_SIZE = height/80
+	
+	love.graphics.setMode( width, height, false, true, 1 )
+	--love.graphics.print("---------------------------------------------------------------")
 	
 	--math.randomseed(1)
-	math.randomseed(os.time()*10000)
+	--math.randomseed(os.time()*10000)
 	grid = getNewGrid()
-	for x = 0,width/10 do
+	for x = 0,width/CELL_SIZE do
 		grid[x] = {}
-		for y = 0,height/10 do
+		for y = 0,height/CELL_SIZE do
 			if x > 20 and x < 40 and y > 20 and y < 40 then
 				--girid[x][y] = 0;			
 				--grid[x][y] = math.random(0,1)
@@ -20,8 +25,8 @@ function love.load()
 	end
 
 	colorGrid = getNewGrid()
-	for x = 1,width/10 do
-		for y = 1,height/10 do
+	for x = 1,width/CELL_SIZE do
+		for y = 1,height/CELL_SIZE do
 			colorGrid[x][y] = 100
 		end
 	end
@@ -36,9 +41,9 @@ end
 function getNewGrid()
 	local newGrid = {}
 	
-	for x = 1,width/10 do
+	for x = 0,width/CELL_SIZE do
 		newGrid[x] = {}
-		for y = 1,height/10 do
+		for y = 0,height/CELL_SIZE do
 			newGrid[x][y] = 0		
 		end
 	end
@@ -47,14 +52,14 @@ function getNewGrid()
 end
 
 function love.update(dt)
-	love.timer.sleep(10)
+	love.timer.sleep(100)
 	
 	local newGrid = getNewGrid()
 	
 	
 	
-	for x = 1, width/10 do
-		for y = 1, height/10 do
+	for x = 0, width/CELL_SIZE do
+		for y = 0, height/CELL_SIZE do
 			oldVal = grid[x][y]
 			ln = getNeighbours(x,y)
 			
@@ -72,10 +77,10 @@ function love.update(dt)
 	if love.mouse.isDown('l') then
 		local x = love.mouse.getX()
 		local y = love.mouse.getY()
-		x = x - (x%10) 
-		y = y - (y%10)
+		x = x - (x%CELL_SIZE) 
+		y = y - (y%CELL_SIZE)
 		--love.graphics.rectangle('fill', x, y, 10, 10)
-		grid[x/10][y/10] = 1
+		grid[x/CELL_SIZE][y/CELL_SIZE] = 1
 	end
 	grid = newGrid
 end
@@ -109,21 +114,21 @@ end
 
 function love.draw()
 	
-	love.graphics.setColor( 255, 255, 255, 255 )
-	for x = 1,800 do
-		love.graphics.line(x * 10, 0, x * 10 , height) 
+	love.graphics.setColor( 50, 50, 50, 255 )
+	for x = 0,height do
+		love.graphics.line(x * CELL_SIZE, 0, x * CELL_SIZE , height) 
 	end
-	for y = 1,600 do
-		love.graphics.line(0, y * 10, width, y * 10)
+	for y = 0,width do
+		love.graphics.line(0, y * CELL_SIZE, width, y * CELL_SIZE)
 	end
 	
-	for x = 1,width/10 do
-		for y = 1,height/10 do
+	for x = 0,width/CELL_SIZE do
+		for y = 0,height/CELL_SIZE do
 			if grid[x][y] == 1 then
 				color = colorGrid[x][y]
 				love.graphics.print(color, 60,60)
 				love.graphics.setColor( color, color, color, 255 )
-				love.graphics.rectangle('fill', x * 10, y * 10, 10, 10)
+				love.graphics.rectangle('fill', x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
 				love.graphics.setColor( 0, 0, 0, 255 )
 			end			
 			-- love.graphics.polygon(fill, x, y, x + 10, y + 10)
@@ -132,9 +137,9 @@ function love.draw()
 	
     local x = love.mouse.getX()
     local y = love.mouse.getY()
-	x = x - (x%10) 
-	 y = y - (y%10)
-	love.graphics.print("The mouse is at (" .. x .. "," .. y .. ")", 50, 50)
+	x = x - (x%CELL_SIZE) 
+	 y = y - (y%CELL_SIZE)
+	--love.graphics.print("The mouse is at (" .. x .. "," .. y .. ")", 50, 50)
 	--love.graphics.print("Height and width are (" .. height .. "," .. width .. ")", 50, 50)
 	-- love.graphics.rectangle('fill', 10, 10, 10, 10)
 	-- love.graphics.polygon('fill', 0, 0, 0, 10, 10, 10, 10, 10)
@@ -144,13 +149,13 @@ end
 
 function love.mousereleased(x, y, button)
 		
-	 x = x - (x%10) 
-	 y = y - (y%10)
+	 x = x - (x%CELL_SIZE) 
+	 y = y - (y%CELL_SIZE)
 		--love.graphics.print("The mouse is at (" .. x .. "," .. y .. ")", 50, 50)
 	if button == 'l' then
 		--grid[x][y] = 1
 		love.graphics.rectangle('fill', x, y, 10, 10)
-		grid[x/10][y/10] = 1
+		grid[x/CELL_SIZE][y/CELL_SIZE] = 1
       --fireSlingshot(x,y) -- this totally awesome custom function is defined elsewhere
 	  
 	end
